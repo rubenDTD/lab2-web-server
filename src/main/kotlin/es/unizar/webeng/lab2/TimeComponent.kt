775fbf00d85ad149a53b09e8c9d2e4def6b05052
rubenDTD <736650@unizar.es>
2022-10-06 16:52:00 +0200

    Primary tasks

---
+++ src/main/kotlin/es/unizar/webeng/lab2/TimeComponent.kt
@@ -0,0 +1,25 @@
+package es.unizar.webeng.lab2
+
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+
+data class TimeDTO(val time: LocalDateTime)
+
+interface TimeProvider {
+    fun now(): LocalDateTime
+}
+
+@Service
+class TimeService: TimeProvider {
+    override fun now() = LocalDateTime.now()
+}
+
+fun LocalDateTime.toDTO() = TimeDTO(time = this)
+
+@RestController
+class TimeController(val service: TimeProvider) {
+    @GetMapping("/time")
+    fun time() = service.now().toDTO()
+}
